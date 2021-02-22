41dc76ebba8ebe2bc295ea8e8f8e0f6784ad8824
iamnasrin <nasrin_solati@yahoo.com>
2021-02-22 12:12:17 +0100

    JS basic

---
+++ script.js
@@ -0,0 +1,102 @@
+fighters = [{
+        id: 0,
+        name: 'Smart and ripped monkey',
+        speciality: 'rip your face off',
+        addFighter: function() {
+            //Write a function that adds your fighter to your list 
+            //both in innerHTML and the empty list here
+
+        }
+    },
+    {
+        id: 1,
+        name: 'Zombie snake',
+        speciality: 'bite',
+        addFighter: function() {
+            //Write a function that adds your fighter to your list 
+            //both in innerHTML and the empty list here
+
+        }
+    },
+    {
+        id: 2,
+        name: 'Sociopathic ghost',
+        speciality: 'manipulate',
+        addFighter: function() {
+            //Write a function that adds your fighter to your list 
+            //both in innerHTML and the empty list here
+
+        }
+    },
+    {
+        id: 3,
+        name: 'A fast Frankenstein',
+        speciality: 'like regular Frankenstein, but fast',
+        addFighter: function() {
+            //Write a function that adds your fighter to your list 
+            //both in innerHTML and the empty list here
+
+        }
+    },
+    {
+        id: 4,
+        name: 'Count Dracula',
+        speciality: 'sleep, spook, drink blood, repeat',
+        addFighter: function() {
+            //Write a function that adds your fighter to your list 
+            //both in innerHTML and the empty list here
+
+        }
+    },
+    {
+        id: 5,
+        name: 'Your crazy nazi aunt',
+        speciality: 'make people uncomfortable',
+        addFighter: function() {
+            //Write a function that adds your fighter to your list 
+            //both in innerHTML and the empty list here
+
+        }
+    },
+    {
+        id: 6,
+        name: 'A kid high on candy',
+        speciality: 'give headaches',
+        addFighter: function() {
+            //Write a function that adds your fighter to your list 
+            //both in innerHTML and the empty list here
+
+        }
+    },
+    {
+        id: 7,
+        name: 'That girl from Grudge',
+        speciality: 'spook',
+        addFighter: function() {
+            //Write a function that adds your fighter to your list 
+            //both in innerHTML and the empty list here
+
+        }
+    },
+]
+
+let yourFighters = [];
+
+//Part 1
+//Create an eventlistener to fighter buttons
+//Call the fighters object-model
+
+
+
+//Part 2
+//Write object methods
+
+// Add selected fighter to the list 'yourFighters'
+// Enter the name in a <li> in the HTML under 'Your Fighters'
+
+
+
+
+//Some next level shit
+// After clicking on a selection from the left list,
+// then that object should disappear from that list and from the HTML
